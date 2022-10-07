304c0a5cd461424308cd220b905d683ea5d845ff
Kan HUANG <kan.huang@connect.ust.hk>
2022-10-07 18:07:07 +0800

    add id for items and entities

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -29,36 +29,42 @@ def load_bio_dataset(bio_dir, dataset_name="MSRA dataset"):
     return dataset_splits
 
 
-def to_doccano():
+def to_doccano_format():
 
     msra_bio_dir = "data/msra_bio"
     dataset_splits = load_bio_dataset(msra_bio_dir)
 
     msra_doccano_dir = "data/msra_doccano"
-
+    global_item_id = 0
+    global_entity_id = 0
     for phase in ["train", "test"]:
         new_filepath = os.path.join(msra_doccano_dir, f"{phase}.jsonl")
         print(f'Writing dataset_split {phase} into file: {new_filepath}...')
         with open(new_filepath, 'w', encoding="utf-8") as output_file:
             for (words, tags) in dataset_splits[phase]:
+                global_item_id += 1
+                item_id = global_item_id
                 # text = ' '.join(words)
                 text = ''.join(words)
                 entities = bio_decode(words, tags)
                 new_entities = []
                 for entity in entities:
+                    global_entity_id += 1
                     new_entities.append({
                         "entity": entity.entity,
+                        "id": global_entity_id,
                         "label": entity.tag,
                         "start_offset": entity.begin,
                         "end_offset": entity.end
                     })
 
-                item = {"text": text, "entities": new_entities, "relations": []}
+                item = {"id": item_id, "text": text,
+                        "entities": new_entities, "relations": []}
                 output_file.write(json.dumps(item, ensure_ascii=False) + '\n')
 
 
 def main():
-    to_doccano()
+    to_doccano_format()
 
 
 if __name__ == "__main__":
